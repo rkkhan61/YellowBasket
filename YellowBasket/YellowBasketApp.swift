83bea261df77f89e3299c26d9a7067974e54e968
Raid Khan <raidkhxn@gmail.com>
2026-03-17 21:40:30 +0000

    Initial Commit

---
+++ YellowBasket/YellowBasketApp.swift
@@ -0,0 +1,17 @@
+//
+//  YellowBasketApp.swift
+//  YellowBasket
+//
+//  Created by Raid Khan on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct YellowBasketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YellowBasketTests/YellowBasketTests.swift
@@ -0,0 +1,17 @@
+//
+//  YellowBasketTests.swift
+//  YellowBasketTests
+//
+//  Created by Raid Khan on 3/17/26.
+//
+
+import Testing
+@testable import YellowBasket
+
+struct YellowBasketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
